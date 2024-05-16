97ba3816c833a5bfb73a16f4452a091b7b605b7a
danny <kdmunoz@sudamericano.edu.ec>
2024-05-16 10:47:29 -0500

    :)

---
+++ 2.1 Comunicacion/TC/Infografia ciclo de vida Angular/___Infografia ciclo de vida Angular__.md
@@ -0,0 +1,3 @@
+___Infografia ciclo de vida Angular__
+
+[Link a la infografia](https://www.figma.com/board/DrgipJrHcs78sua2MoxKIh/CICLO-DE-VIDA-DE-ANGULAR?node-id=0%3A1&t=liy8sSzaOcYbJAkZ-1)
--- semana1/TA/holaMundo.net/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ semana1/TA/holaMundo.net/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201f2abea9d75a059b100188acecfee336a20446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bc193379b5a8c48688ac573063a130b44fd825")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
